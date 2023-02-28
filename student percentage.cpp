fb9210a5966f25865ffb68f493659b3c2309bf7f
Rakshit Gupta <95240061+Rakshitgupta9@users.noreply.github.com>
2023-02-28 22:21:21 +0530

    Update student percentage.cpp

--- student percentage.cpp
+++ student percentage.cpp
@@ -1,65 +1,66 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
 class Student {
-public:
+private:
     int rollNo;
     string name;
-    string subject1, subject2, subject3;
-    int maxMarks = 100, minMarks = 40; // set max marks to 100 and min marks to 40
-    int marks1, marks2, marks3;
-
-    void displayResult() {
-        cout << "Roll No: " << rollNo << endl;
-        cout << "Name: " << name << endl;
-        cout << "Subject 1: " << subject1 << "\tMarks: " << marks1 << endl;
-        cout << "Subject 2: " << subject2 << "\tMarks: " << marks2 << endl;
-        cout << "Subject 3: " << subject3 << "\tMarks: " << marks3 << endl;
-        cout << "Percentage: " << calculatePercentage() << "%" << endl;
-        cout << endl;
+    string subjects[3];
+    int maxMarks[3];
+    int minMarks[3];
+    int obtainMarks[3];
+public:
+    void setData(int rollNo, string name, string sub1, string sub2, string sub3) {
+        this->rollNo = rollNo;
+        this->name = name;
+        subjects[0] = sub1;
+        subjects[1] = sub2;
+        subjects[2] = sub3;
+        for(int i=0; i<3; i++) {
+            cout << "Enter max marks for " << subjects[i] << ": ";
+            cin >> maxMarks[i];
+            cout << "Enter min marks for " << subjects[i] << ": ";
+            cin >> minMarks[i];
+            cout << "Enter obtain marks for " << subjects[i] << ": ";
+            cin >> obtainMarks[i];
+        }
     }
 
-private:
-    float calculatePercentage() {
-        float totalMarks = marks1 + marks2 + marks3;
-        float maxTotalMarks = maxMarks * 3;
-        return (totalMarks / maxTotalMarks) * 100;
+    void showResult() {
+        cout << "Roll No.: " << rollNo << endl;
+        cout << "Name: " << name << endl;
+        for(int i=0; i<3; i++) {
+            cout << subjects[i] << ":\t" << obtainMarks[i] << "/" << maxMarks[i] << endl;
+        }
+        float totalMarks = obtainMarks[0] + obtainMarks[1] + obtainMarks[2];
+        float percentage = (totalMarks / (maxMarks[0] + maxMarks[1] + maxMarks[2])) * 100;
+        cout << "Percentage: " << percentage << "%" << endl << endl;
     }
 };
 
 int main() {
-    int numStudents;
-    cout << "Enter the number of students: ";
-    cin >> numStudents;
+    Student students[3];
 
-    Student students[numStudents];
+    for(int i=0; i<3; i++) {
+        int rollNo;
+        string name, sub1, sub2, sub3;
 
-    // Input data for each student
-    for (int i = 0; i < numStudents; i++) {
-        cout << "Enter details for student " << i+1 << endl;
-        cout << "Roll No: ";
-        cin >> students[i].rollNo;
+        cout << "Enter details of student " << i+1 << endl;
+        cout << "Roll No.: ";
+        cin >> rollNo;
         cout << "Name: ";
-        cin >> students[i].name;
-        cout << "Subject 1: ";
-        cin >> students[i].subject1;
-        cout << "Marks obtained for Subject 1: ";
-        cin >> students[i].marks1;
-        cout << "Subject 2: ";
-        cin >> students[i].subject2;
-        cout << "Marks obtained for Subject 2: ";
-        cin >> students[i].marks2;
-        cout << "Subject 3: ";
-        cin >> students[i].subject3;
-        cout << "Marks obtained for Subject 3: ";
-        cin >> students[i].marks3;
-        cout << endl;
+        cin.ignore(); // To clear input buffer
+        getline(cin, name);
+        cout << "Enter name of three subjects: ";
+        cin >> sub1 >> sub2 >> sub3;
+
+        students[i].setData(rollNo, name, sub1, sub2, sub3);
     }
 
-    // Display results for each student
-    for (int i = 0; i < numStudents; i++) {
-        students[i].displayResult();
+    cout << endl << "Result of all students:" << endl << endl;
+
+    for(int i=0; i<3; i++) {
+        students[i].showResult();
     }
 
     return 0;
